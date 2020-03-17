7e669031a6a6529097454f43b220e64ea3992757
noel <noel.araujo.moreira@gmail.com>
2020-03-17 15:23:40 -0300

    Convert into matrix multiplications
    I cannot understand why, but it works

--- QPC_optimizations.jl
+++ QPC_optimizations.jl
@@ -15,8 +15,10 @@ function QPC_v2!(du, u, p, t)
 	Δ₀ = p[7]
 	Γ = p[8]
 	k₀ = p[9]
-	Exclu3 = p[10]
+	exclusion3D = p[10]
 	Gconj = p[11]
+	exclusionDiagonal = p[12]
+	jₐᵤₓ,mₐᵤₓ,kₐᵤₓ = 1,2,3
 
 	σ  = @view u[1:N]
     σᶻ = @view u[N+1:2*N]
@@ -30,85 +32,67 @@ function QPC_v2!(du, u, p, t)
 	σσᶻ = transpose(σᶻσ)
 	σ⁺σᶻ = σᶻσ'
 
-	σ⁺σσ  = truncation(σ⁺, σ, σ, σ⁺σ, σ⁺σ, σσ, N)
-	σᶻσᶻσ = truncation(σᶻ, σᶻ,σ, σᶻσᶻ,σᶻσ, σᶻσ, N)
-	σ⁺σσᶻ = Exclu3.*truncation(σ⁺, σ, σᶻ,σ⁺σ, σ⁺σᶻ, σσᶻ, N)
-	σᶻσσ  = truncation(σᶻ, σ, σ, σᶻσ, σᶻσ, σσ, N)
-	σ⁺σᶻσ = truncation(σ⁺, σᶻ, σ, σ⁺σᶻ, σ⁺σ, σᶻσ, N)
+	σ⁺σσ  = exclusao3D.*truncation(σ⁺, σ, σ, σ⁺σ, σ⁺σ, σσ, N)
+	σᶻσᶻσ = exclusao3D.*truncation(σᶻ, σᶻ,σ, σᶻσᶻ,σᶻσ, σᶻσ, N)
+	σ⁺σσᶻ = exclusao3D.*truncation(σ⁺, σ, σᶻ,σ⁺σ, σ⁺σᶻ, σσᶻ, N)
+	σᶻσσ  = exclusao3D.*truncation(σᶻ, σ, σ, σᶻσ, σᶻσ, σσ, N)
+	σ⁺σᶻσ = exclusao3D.*truncation(σ⁺, σᶻ, σ, σ⁺σᶻ, σ⁺σ, σᶻσ, N)
 	σᶻσ⁺σ = σ⁺σσᶻ
 
 	# Implementation of Eq (6)-(11) in PDF
 	# Eq (6)
-	dₜ_σ = zeros(ComplexF64, N)
-    for j = 1:N
-		du[j] = ( (im*Δ₀ - Γ/2)*σ[j]
-					+ (Ω⁺[j]/2).*σᶻ[j]
-					+ (Γ/2)*sum(m ≠ j ? G[j,m]*σᶻσ[j,m] : 0 for m=1:N)
-		)
-	end
+	du[1:N] .= (im*Δ₀-1/2).*σ .+ (Ω⁺/2).*σᶻ .+ (Γ/2).*vec(sum(exclusaoDiagonal.*(G.*σᶻσ),dims=2))
 
 	# Eq (7)
-    for j = 1:N
-		du[j+N] = ( 	  Ω⁻[j]*σ[j] + conj( Ω⁻[j]*σ[j] )
-                        - Γ*(1 + σᶻ[j])
-                        - Γ*sum(m ≠ j ? G[j,m]*σ⁺σ[j,m] + conj(G[j,m]*σ⁺σ[j,m]) : 0 for m=1:N)
-        )
-
-    end
+	du[N+1:2*N] .= Ω⁻.*σ .+ conj( Ω⁻.*σ ) .- Γ*(1 .+ σᶻ) .- Γ.*vec(sum(exclusaoDiagonal.*(G.*σ⁺σ) + conj.(exclusaoDiagonal.*(G.*σ⁺σ)),dims=2))
 
 	# Eq (8)
-	for j = 1:N
-        m_range = deleteat!(collect(1:N), j)
-        for m in m_range
-				du[2N + j + (m-1)*N] = ( (im*Δ₀ - 3Γ/2)*σᶻσ[j,m]
-								- Γ*σ[m]
-								+ ( Ω⁻[j]*σσ[j,m] - Ω⁺[j]*σ⁺σ[j,m] + 0.5*Ω⁺[m]*σᶻσᶻ[j,m] )
-								- Γ*sum(((k≠j)*(k≠m)) ? G[j,k].*σ⁺σσ[j,m,k] + Gconj[j,k]*σ⁺σσ[k,m,j] : 0 for k=1:N)
-								+ 0.5Γ*sum((k≠j)&&(k≠m) ? G[m,k]*σᶻσᶻσ[m,j,k] : 0 for k=1:N)
-								- Γ*Γⱼₘ[j,m]*σσᶻ[j,m] - 0.5Γ*Gconj[j,m]*σ[j]
-				)
-		end
-	end
+	du[2*N+1:2*N+N^2] .= reshape( ( (im*Δ₀ - 3Γ/2).*σᶻσ
+					.- Γ*ones(N).*transpose(σ)
+					.+ σσ.*Ω⁻ .- σ⁺σ.*Ω⁺ .+ 0.5.*σᶻσᶻ.*transpose(Ω⁺)
+					.- Γ.*sum( reshape(G,(N,1,N)).*σ⁺σσ .+ (reshape(Gconj,(N,1,N)).*permutedims(σ⁺σσ,[kₐᵤₓ,mₐᵤₓ,jₐᵤₓ])), dims=3)
+					.+(Γ/2).*transpose(reshape(sum(G.*permutedims(σᶻσᶻσ,[jₐᵤₓ,kₐᵤₓ,mₐᵤₓ]), dims=2),(N,N)))
+					.- Γ*σσᶻ.*Γⱼₘ - (Γ/2).*Gconj.*σ
+					),(N^2)
 
+	)
 	# Eq (9)
-	for j = 1:N
-        m_range = deleteat!(collect(1:N), j)
-        for m in m_range
-				du[2*N+N^2 + j + (m-1)*N] = ( -Γ*σ⁺σ[j,m] - 0.5*( Ω⁻[j]*σᶻσ[j,m]  - Ω⁺[m]*σ⁺σᶻ[j,m] )
-								+ 0.5Γ*sum((k≠j)&&(k≠m) ? Gconj[j,k]*σ⁺σσᶻ[k,m,j] + G[m,k]*σ⁺σσᶻ[j,k,m] : 0 for k=1:N)
-								+ 0.25Γ*(G[j,m]*σᶻ[m] + Gconj[j,m]*σᶻ[j] )
-								+ 0.5Γ*Γⱼₘ[j,m]*σᶻσᶻ[j,m]
-				)
-		end
-	end
-
+	du[2*N+N^2+1:2*N+2*N^2] .= reshape(
+								(  -Γ.*σ⁺σ .- 0.5*( σᶻσ.*Ω⁻ - σ⁺σᶻ.*transpose(Ω⁺) )
+									.+ 0.5(reshape(sum(reshape(Gconj,(N,1,N)).*permutedims(σ⁺σσᶻ, [kₐᵤₓ,mₐᵤₓ,jₐᵤₓ]), dims=3), (N,N)).+reshape(sum(reshape(G,(1,N,N)).*permutedims(σ⁺σσᶻ, [jₐᵤₓ,kₐᵤₓ,mₐᵤₓ]), dims=3), (N,N)))
+									.+ 0.25Γ*(transpose(σᶻ).*G + σᶻ.*Gconj )
+									.+ 0.5Γ*Γⱼₘ.*σᶻσᶻ
+								), (N^2)
+								)
 	# Eq (10)
-	for j = 1:N
-        m_range = deleteat!(collect(1:N), j)
-        for m in m_range
-				du[2*N+2*N^2 + j + (m-1)*N] = ( (2im*Δ₀ - Γ)*σσ[j,m] + 0.5(  Ω⁺[j]*σᶻσ[j,m]  + Ω⁺[m]*σᶻσ[m,j] )
-								+ 0.5Γ*sum((k≠j)&&(k≠m) ? G[j,k]*σᶻσσ[j,m,k] + G[m,k]*σᶻσσ[m,j,k] : 0 for k=1:N)
+	du[2*N+1+2*N^2:2*N+3*N^2] .= reshape(
+				( (2im*Δ₀ - Γ)*σσ .+ 0.5( σᶻσ.*Ω⁺ .+  transpose(σᶻσ).*transpose(Ω⁺))
+				.+ (Γ/2)sum(reshape(G,(N,1,N)).*(σᶻσσ) .+ reshape(G,(1,N,N)).*permutedims(σᶻσσ,[mₐᵤₓ,jₐᵤₓ,kₐᵤₓ]) ,dims=3)
 				)
-		end
-	end
-
+				,(N^2))
 	# Eq (11)
-	for j = 1:N
-        m_range = deleteat!(collect(1:N), j)
-        for m in m_range
-				du[2*N+3*N^2 + j + (m-1)*N] = (-Γ*( σᶻ[j] + σᶻ[m] + 2σᶻσᶻ[j,m])
-								+  Ω⁻[j]*σᶻσ[m,j]  + Ω⁻[m]*σᶻσ[j,m] + conj( Ω⁻[j]*σᶻσ[m,j]  + Ω⁻[m]*σᶻσ[j,m] )
-								# - 2Γ*real(sum((k≠j)&&(k≠m) ? G[j,k]*σ⁺σᶻσ[j,m,k] + G[m,k]*σᶻσ⁺σ[j,m,k] : 0 for k=1:N))
-								- 2Γ*real(sum((k≠j)&&(k≠m) ? G[j,k]*σ⁺σᶻσ[j,m,k] : 0 for k=1:N) + sum(transpose(G[:,k]).*permutedims(σ⁺σσᶻ, [3 1 2])[:,:,k] for k=1:N, dims=3)[j,m])
-								+ 4Γ*Γⱼₘ[j,m]*real( σ⁺σ[j,m])
-				)
-		end
-	end
+	du[2*N+1+3*N^2:2*N+4*N^2] .= reshape(
+		(
+			.-Γ*(σᶻ.*ones(1,N) + ones(N,1).*transpose(σᶻ) + 2σᶻσᶻ)
+			.+ transpose(σᶻσ).*Ω⁻ .+ σᶻσ.*transpose(Ω⁻) .+ conj.(transpose(σᶻσ).*Ω⁻ .+ σᶻσ.*transpose(Ω⁻))
+			.-2Γ*reshape(real(sum(reshape(G,(N,1,N)).*(σ⁺σᶻσ) .+ reshape(G,(1,N,N)).*permutedims(σ⁺σσᶻ,[kₐᵤₓ,jₐᵤₓ,mₐᵤₓ]) ,dims=3)),(N,N))
+			+ 4Γ*Γⱼₘ.*real(σ⁺σ)
+		), (N^2)
+	)
+	return nothing
 end
 
 
+
+v21 = deepcopy(u₀_off)
+dv21 = deepcopy(u₀_off)
+QPC_v2!(dv21, v21, p_on_v2,0.0)
+
+@profiler solve(prob_off,RK4(), dt=(1/2)^4, adaptive=false)
+
+
 function truncation(A, B, C, AB, AC, BC, N)
-	# N = length(A)
+	N = length(A)
 	f = (-2.0.*reshape(A*transpose(B),(N,N,1)).*reshape(C,(1,1,N))
 			+ reshape(BC,(1,N,N)).*reshape(A,(N,1,1))
 	        + reshape(AB,(N,N,1)).*reshape(C,(1,1,N))
@@ -117,7 +101,6 @@ function truncation(A, B, C, AB, AC, BC, N)
 end
 
 # ------------- parameters -------------
-
 X =  [-0.1141,  -0.0507,   -0.3428,   -0.1587,    0.1385]
 Y =  [-0.0775,    0.6600,   -0.2585,   -0.6381,    0.7721]
 Z =  [0.4698,   -0.0815,   -0.5313,   -0.3636,   -0.1457]
@@ -137,8 +120,9 @@ G .= -2G
 Gconj = conj.(G)
 Γⱼₘ =  real.(G)
 
-offd = ones(N, N) .- LinearAlgebra.I(N)
-Exclu3 = (offd .* reshape(offd,(N,1,N))).*reshape(offd,(1,N,N))
+exclusionDiagonal = ones(Int8,N,N) .- I(N)
+exclusion3D = ones(Int8, N, N) .- LinearAlgebra.I(N)
+exclusion3D = (exclusion3D .* reshape(exclusion3D,(N,1,N))).*reshape(exclusion3D,(1,N,N))
 
 
 Ω₀ = 10.0
@@ -154,8 +138,9 @@ push!(p_on_v2, Ω⁻)
 push!(p_on_v2, Δ₀)
 push!(p_on_v2, Γ)
 push!(p_on_v2, k₀)
-push!(p_on_v2, Exclu3)
+push!(p_on_v2, exclusion3D)
 push!(p_on_v2, Gconj)
+push!(p_on_v2, exclusionDiagonal)
 
 Ω₀ = 0.0
 Ω⁺ = im*Ω₀.*exp.( im.*[dot(k₀, r[i,:]) for i=1:N] )
@@ -170,25 +155,32 @@ push!(p_off_v2, Ω⁻)
 push!(p_off_v2, Δ₀)
 push!(p_off_v2, Γ)
 push!(p_off_v2, k₀)
-push!(p_off_v2, Exclu3)
+push!(p_off_v2, exclusion3D)
 push!(p_off_v2, Gconj)
+push!(p_off_v2, exclusionDiagonal)
 
 u₀_on = zeros(ComplexF64, 2*N+4*N^2)
 u₀_on[N+1:2*N] .= -1
 u₀_on[2*N+3*N^2+1:2*N+4*N^2] .= +1
 u₀_on[2*N+3*N^2+1:N+1:2*N+4*N^2] .= 0.0
 
+# offd = ones(N, N) .- LinearAlgebra.I(N)
+# Exclu3 = (offd .* reshape(offd,(N,1,N))).*reshape(offd,(1,N,N))
+
+
+
+
 # -------------------------------------------------
 tspan_on = (0.0, 100.0)
 prob_on = ODEProblem(QPC_v2!,u₀_on,tspan_on,p_on_v2)
-@time sol_on = DifferentialEquations.solve(prob_on)
+@time sol_on = DifferentialEquations.solve(prob_on, RK4(), dt=(1/2)^4, adaptive=false)
 population_on = 0.5 .+ 0.5*[ real(mean(sol_on.u[i][N+1:2*N])) for i in eachindex(sol_on.u)]
 # plot(sol_on.t, abs.(population_on), framestyle=:box, xlabel="t",label="", ylabel="0.5 + 0.5 <Population>")
 
 tspan_off = (100.0, 140.0)
 u₀_off = sol_on.u[end]
 prob_off = ODEProblem(QPC_v2!,u₀_off,tspan_off,p_off_v2)
-@time sol_off = DifferentialEquations.solve(prob_off)
+@time sol_off = DifferentialEquations.solve(prob_off,RK4(), dt=(1/2)^4, adaptive=false)
 population_off = 0.5 .+ 0.5*[ real(mean(sol_off.u[i][N+1:2*N])) for i in eachindex(sol_off.u)]
 # plot!(sol_off.t, abs.(population_off), framestyle=:box, xlabel="t",label="", ylabel="0.5 + 0.5 <Population>")
 # plot!(yscale=:log10)
